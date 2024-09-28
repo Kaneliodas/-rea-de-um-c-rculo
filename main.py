fd5eee115e683c810417a6b230099c55b4f9c03e
João Vitor Sousa Ramos <79048652+Kaneliodas@users.noreply.github.com>
2024-09-28 17:26:05 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,4 @@
+#Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+raio = float(input("Qual o raio do círculo em questão? "))
+diam = str((raio**2)*3.14)
+print("A área do círculo em questão é: " + diam)
